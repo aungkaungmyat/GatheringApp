bfddd442f2ca8f56a1fc6e62272ee59db92fdb17
aungkaungmyat <aungkaungmyat.olaf@gmail.com>
2017-05-20 21:13:48 +0630

    Finished keep tracking of current user

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var bodyParser = require('body-parser')
 var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+var uname;
+
 app.set('view engine', 'ejs');
 app.use('/assets' , express.static('assets'));
 
@@ -11,8 +13,13 @@ app.get('/',function(req,res){
 });
 
 app.post('/select',urlencodedParser,function(req,res){
-  console.log(req.body);
+  uname = req.body.usrname;
+  console.log(uname);
   res.render('select', {userinfo : req.body} );
 })
+
+app.get('/groupCreate' , function(req,res){
+  res.render('groupCreate' , {uname : uname})
+});
 
 app.listen(3000);
