b75f35dd284d6712493f5e4c8299ab95a5902468
aungkaungmyat <aungkaungmyat.olaf@gmail.com>
2017-05-21 23:29:44 +0630

    replaced global variable with cookies

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-var uname;
+//var uname;
 
 app.set('view engine', 'ejs');
+
+//testing code
+
+
+// set a cookie
+/*app.use(function (req, res, next) {
+  // check if client sent cookie
+  var cookie = req.cookies.cookieName;
+  if (cookie === undefined)
+  {
+    // no: set a new cookie
+    // var randomNumber=Math.random().toString();
+    // randomNumber=randomNumber.substring(2,randomNumber.length);
+    res.cookie('cookieName',uname, { maxAge: 900000, httpOnly: true });
+    console.log('cookie created successfully');
+  }
+  else
+  {
+    // yes, cookie was already present
+    console.log('cookie exists', cookie);
+  }
+  next(); // <-- important!
+});*/
+app.use(cookieParser());
+
 app.use('/assets' , express.static('assets'));
 
 app.get('/',function(req,res){
@@ -13,13 +39,24 @@ app.get('/',function(req,res){
 });
 
 app.post('/select',urlencodedParser,function(req,res){
-  uname = req.body.usrname;
-  console.log(uname);
+  //uname = req.body.usrname;
+
+  // read cookies
+    console.log(req.cookies)
+
+
+
+    // Set cookie
+    res.cookie('cookieName', req.body.usrname) // options is optional
+
+  //console.log(uname);
   res.render('select', {userinfo : req.body} );
 })
 
 app.get('/groupCreate' , function(req,res){
-  res.render('groupCreate' , {uname : uname})
+  console.log(req.cookies);
+  console.log(req.cookies.cookieName);
+  res.render('groupCreate' , {uname : req.cookies.cookieName})
 });
 
 app.listen(3000);
