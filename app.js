cbcfc2309ccfaea3061754785fbd58d284cec8bc
aungkaungmyat <aungkaungmyat.olaf@gmail.com>
2017-05-22 00:09:18 +0630

    Changed cookie parser to session cookie

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var cookieParser = require('cookie-parser');
+var session = require("cookie-session");
 var bodyParser = require('body-parser');
 var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -10,27 +10,10 @@ app.set('view engine', 'ejs');
 
 //testing code
 
+app.use(session({
+    secret: 'what does this do?' // great question! We will come back to that, but we need it for now!
+}))
 
-// set a cookie
-/*app.use(function (req, res, next) {
-  // check if client sent cookie
-  var cookie = req.cookies.cookieName;
-  if (cookie === undefined)
-  {
-    // no: set a new cookie
-    // var randomNumber=Math.random().toString();
-    // randomNumber=randomNumber.substring(2,randomNumber.length);
-    res.cookie('cookieName',uname, { maxAge: 900000, httpOnly: true });
-    console.log('cookie created successfully');
-  }
-  else
-  {
-    // yes, cookie was already present
-    console.log('cookie exists', cookie);
-  }
-  next(); // <-- important!
-});*/
-app.use(cookieParser());
 
 app.use('/assets' , express.static('assets'));
 
@@ -39,24 +22,15 @@ app.get('/',function(req,res){
 });
 
 app.post('/select',urlencodedParser,function(req,res){
-  //uname = req.body.usrname;
 
-  // read cookies
-    console.log(req.cookies)
+    req.session.uname = req.body.usrname;
 
 
-
-    // Set cookie
-    res.cookie('cookieName', req.body.usrname) // options is optional
-
-  //console.log(uname);
   res.render('select', {userinfo : req.body} );
 })
 
 app.get('/groupCreate' , function(req,res){
-  console.log(req.cookies);
-  console.log(req.cookies.cookieName);
-  res.render('groupCreate' , {uname : req.cookies.cookieName})
+  res.render('groupCreate' , {uname : req.session.uname})
 });
 
 app.listen(3000);
