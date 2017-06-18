e989737434e96ccc912cae0a891765a0206f6edf
aungkaungmyat <amyat@purdue.edu>
2017-06-18 14:13:09 +0630

    animated transitions

--- test.js
+++ test.js
@@ -1,11 +1,16 @@
 new Vue({
   el: '#vue-app',
   data: {
-    number: 1
+    number: 1,
+    nextNumber: 0,
+    show: true
   },
   methods:{
     add:function(inc){
       this.number += inc;
+      this.nextNumber = this.number;
+      this.show = !this.show;
+      // console.log(this.show);
     }
   }
 });
