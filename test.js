badca0f5693b14de1e15ed338167a11bb756b8ef
aungkaungmyat <amyat@purdue.edu>
2017-06-24 20:03:00 +0630

    styled the map

--- test.js
+++ test.js
@@ -175,12 +175,12 @@ function showPosition(position) {
       icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
     });
     google.maps.event.addListener(marker, 'dragend', function(evt){
-    document.getElementById('current').innerHTML = '<p>Marker dropped: Current Lat: ' + evt.latLng.lat().toFixed(7) + ' Current Lng: ' + evt.latLng.lng().toFixed(7) + '</p>';
+    // document.getElementById('current').innerHTML = '<p>Marker dropped: Current Lat: ' + evt.latLng.lat().toFixed(7) + ' Current Lng: ' + evt.latLng.lng().toFixed(7) + '</p>';
     document.getElementById('lat').value = evt.latLng.lat().toFixed(7);
     document.getElementById('lng').value = evt.latLng.lng().toFixed(7);
 });
 
-    google.maps.event.addListener(marker, 'dragstart', function(evt){
-    document.getElementById('current').innerHTML = '<p>Currently dragging marker...</p>';
-});
+//     google.maps.event.addListener(marker, 'dragstart', function(evt){
+//     document.getElementById('current').innerHTML = '<p>Currently dragging marker...</p>';
+// });
 };
