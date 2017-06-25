0daebb6f711c4664650bf5ba97fb0bf77d8f73f8
aungkaungmyat <amyat@purdue.edu>
2017-06-25 23:54:25 +0630

    cleaned up codes a bit

--- controllers/groupCreate.js
+++ controllers/groupCreate.js
@@ -206,21 +206,18 @@ $(document).ready(function(){
 
 var uluru;
 
-// window.onload = function(){
-//   navigator.geolocation.getCurrentPosition(showPosition);
-// };
+
 function startMap(){
   navigator.geolocation.getCurrentPosition(showPosition);
 }
 
 function showPosition(position) {
-  // var lat = position.coords.latitude;
-  // var lng = position.coords.longitude;
+
     uluru = {lat: position.coords.latitude, lng: position.coords.longitude};
     document.getElementById('lat').value = position.coords.latitude;
-    // $('input[name="lat"]').val(position.coords.latitude);
+
     document.getElementById('lng').value = position.coords.longitude;
-    // $('input[name="lng"]').val(position.coords.longitude);
+
 
 
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -235,12 +232,9 @@ function showPosition(position) {
       icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
     });
     google.maps.event.addListener(marker, 'dragend', function(evt){
-    // document.getElementById('current').innerHTML = '<p>Marker dropped: Current Lat: ' + evt.latLng.lat().toFixed(7) + ' Current Lng: ' + evt.latLng.lng().toFixed(7) + '</p>';
     document.getElementById('lat').value = evt.latLng.lat().toFixed(7);
     document.getElementById('lng').value = evt.latLng.lng().toFixed(7);
 });
 
-//     google.maps.event.addListener(marker, 'dragstart', function(evt){
-//     document.getElementById('current').innerHTML = '<p>Currently dragging marker...</p>';
-// });
+
 };
